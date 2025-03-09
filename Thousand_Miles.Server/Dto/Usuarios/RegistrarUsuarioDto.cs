9c418e1bee483b44e59f96defa62506563077610
Dev-Vinicius-Borges <dev.vinicius.borges@outlook.com>
2025-03-09 17:23:04 -0300

    Feat(DTO): Criando o DTO para o registro de novos usuarios.

---
+++ Thousand_Miles.Server/Dto/Usuarios/RegistrarUsuarioDto.cs
@@ -0,0 +1,12 @@
+﻿
+namespace ThousandMiles.Server.Dto.Usuarios
+{
+    public class RegistrarUsuarioDto
+    {
+        public string email { get; set; }
+
+        public string senha { get; set; }
+
+        public byte status { get; set; }
+    }
+}
