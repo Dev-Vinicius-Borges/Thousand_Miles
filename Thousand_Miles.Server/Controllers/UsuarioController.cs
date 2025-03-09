08b8944d7110b5685995b3dea9b812f2017bbbe2
Dev-Vinicius-Borges <dev.vinicius.borges@outlook.com>
2025-03-09 19:39:04 -0300

    Feat: Criação das rotas do usuário.

---
+++ Thousand_Miles.Server/Controllers/UsuarioController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ThousandMiles.Server.Dto.Usuarios;
+using ThousandMiles.Server.Models;
+using ThousandMiles.Server.Models.Usuario;
+using ThousandMiles.Server.Services.Usuarios;
+
+namespace ThousandMiles.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioInterface _usuarioInterface;
+        public UsuarioController(IUsuarioInterface usuarioInterface)
+        {
+            _usuarioInterface = usuarioInterface;
+        }
+
+        [HttpPost("Registrar")]
+        public async Task<ActionResult<RespostaModel<UsuarioModel>>> RegistrarUsuario(RegistrarUsuarioDto registrarUsuarioDto)
+        {
+            var registro = await _usuarioInterface.RegistrarUsuario(registrarUsuarioDto);
+            return Ok(registro);
+        }
+
+        [HttpPost("Login")]
+        public async Task<ActionResult<RespostaModel<UsuarioModel>>> LoginDeUsuario(LoginDeUsuarioDto loginDeUsuarioDto)
+        {
+            var login = await _usuarioInterface.LoginDeUsuario(loginDeUsuarioDto);
+            return Ok(login);
+        }
+    }
+}
