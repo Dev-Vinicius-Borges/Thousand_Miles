2ee7d8e48f7997306e8cf4df26a326fa11478f78
Dev-Vinicius-Borges <dev.vinicius.borges@outlook.com>
2025-03-09 17:24:12 -0300

    Feat (DTO): Criando o DTO para atualização de dados do usuário.

---
+++ Thousand_Miles.Server/Dto/Usuarios/AtualizarUsuarioDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ThousandMiles.Server.Dto.Usuarios
+{
+    public class AtualizarUsuarioDto
+    {
+        public int id { get; set; }
+        public string email { get; set; }
+        public string senha { get; set; }
+
+    }
+}
