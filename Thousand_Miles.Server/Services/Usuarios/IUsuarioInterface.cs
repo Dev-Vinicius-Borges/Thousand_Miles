60c3b5d8fc092e2e934ada5f8b795c8f927c96a5
Dev-Vinicius-Borges <dev.vinicius.borges@outlook.com>
2025-03-09 17:25:02 -0300

    Feat: Criação da interface contendo os métodos necessários referente aos usuários.

---
+++ Thousand_Miles.Server/Services/Usuarios/IUsuarioInterface.cs
@@ -0,0 +1,18 @@
+﻿using ThousandMiles.Server.Dto.Usuarios;
+using ThousandMiles.Server.Models;
+using ThousandMiles.Server.Models.Usuario;
+
+namespace ThousandMiles.Server.Services.Usuarios
+{
+    public interface IUsuarioInterface
+    {
+        Task<RespostaModel<UsuarioModel>> BuscarUsuarioPorEmail(string email);
+        Task<RespostaModel<UsuarioModel>> RegistrarUsuario(RegistrarUsuarioDto registrarUsuarioDto);
+
+        Task<RespostaModel<UsuarioModel>> LoginDeUsuario(LoginDeUsuarioDto loginDeUsuarioDto);
+
+        Task<RespostaModel<UsuarioModel>> BuscarUsuarioProId(int id);
+
+        Task<RespostaModel<UsuarioModel>> AtualizarUsuario(AtualizarUsuarioDto atualizarUsuarioDto);
+    }
+}
