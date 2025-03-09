f4da87eace35f2731ea1bf864cdfa42bbb2b9998
Dev-Vinicius-Borges <dev.vinicius.borges@outlook.com>
2025-03-09 17:23:42 -0300

    Feat (DTO): Criando o DTO para login de usuários.

---
+++ Thousand_Miles.Server/Dto/Usuarios/LoginDeUsuarioDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ThousandMiles.Server.Dto.Usuarios
+{
+    public class LoginDeUsuarioDto
+    {
+        public string email { get; set; }
+        public string senha { get; set; }
+    }
+}
