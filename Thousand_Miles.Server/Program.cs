6e4820732867344e46591aabc983c816751a94e9
Dev-Vinicius-Borges <dev.vinicius.borges@outlook.com>
2025-03-09 17:33:00 -0300

    Feat: Criando as intruções para uso da string de conexão e criando a conexão entre a interface e o serviço de usuário.

--- Thousand_Miles.Server/Program.cs
+++ Thousand_Miles.Server/Program.cs
@@ -1,8 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using ThousandMiles.Server.Database;
+using ThousandMiles.Server.Services.Usuarios;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+
+
+builder.Services.AddScoped<IUsuarioInterface, UsuarioService>();
+
+builder.Services.AddDbContext<AppDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 
 var app = builder.Build();
 
