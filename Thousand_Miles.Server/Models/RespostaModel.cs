89970807067633d51e8cb6a8e4df10c32818e27a
Dev-Vinicius-Borges <dev.vinicius.borges@outlook.com>
2025-03-09 17:20:46 -0300

    Feat: Criando os modelos e resposta e construção de JWT.

---
+++ Thousand_Miles.Server/Models/RespostaModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ThousandMiles.Server.Models
+{
+    public class RespostaModel<T>
+    {
+        public T? Dados { get; set; }
+        public int status { get; set; }
+        public string Mensagem { get; set; }
+    
+        public string? Token { get; set; }
+    }
+}
---
+++ Thousand_Miles.Server/Models/TokenModel.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ThousandMiles.Server.Models
+{
+    public class TokenModel
+    {
+        public string Token { get; set; }
+        private readonly string _secretKey;
+
+        public TokenModel(Claim[] newClaims)
+        {
+            _secretKey = Environment.GetEnvironmentVariable("JWT_SECRET_KEY");
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes(_secretKey);
+            var claims = new List<Claim>();
+
+            foreach (Claim claim in newClaims)
+            {
+                claims.Add(claim);
+            }
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddHours(3),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+                Issuer = Environment.GetEnvironmentVariable("JWT_ISSUER"),
+                Audience = Environment.GetEnvironmentVariable("JWT_AUDIENCE")
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+
+            Token = tokenString;
+        }
+
+    }
+
+
+}
