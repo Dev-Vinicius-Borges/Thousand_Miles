9d85134c3dfe5d9d598097fef9daca6235978360
Dev-Vinicius-Borges <dev.vinicius.borges@outlook.com>
2025-03-09 17:19:48 -0300

    Feat: Criando os modelos do banco de dados referentes às reservas.

---
+++ Thousand_Miles.Server/Models/Reservas/ReservaModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+using ThousandMiles.Server.Models.Usuario;
+using ThousandMiles.Server.Models.Veiculo;
+
+namespace ThousandMiles.Server.Models.Reservas
+{
+    public class ReservaModel
+    {
+        [Key]
+        public int id_reserva { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime data_inicio { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime data_devolucao { get; set; }
+
+        public float preco_total { get; set; }
+
+        [JsonIgnore]
+        public SeguroModel seguro { get; set; }
+
+        [JsonIgnore]
+        public UsuarioModel usuario { get; set; }
+
+        [JsonIgnore]
+        public VeiculoModel veiculo { get; set; }
+
+
+    }
+}
---
+++ Thousand_Miles.Server/Models/Reservas/SeguroModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ThousandMiles.Server.Models.Reservas
+{
+    public class SeguroModel
+    {
+        [Key]
+        public int id_seguro { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        public string tipo_seguro { get; set; }
+
+        public string descricao { get; set; }
+
+        public float custo { get; set; }
+    }
+}
