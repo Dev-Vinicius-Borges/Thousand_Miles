542dce88c355849615c6901e2db361f5644b9d12
Dev-Vinicius-Borges <dev.vinicius.borges@outlook.com>
2025-04-07 00:21:56 -0300

    Feat: DTO para criação de endereços.

---
+++ Thousand_Miles.Server/Dto/Enderecos/CriarEnderecoDto.cs
@@ -0,0 +1,14 @@
+﻿using Thousand_Miles.Server.Dto.Bairros;
+
+namespace Thousand_Miles.Server.Dto.Enderecos
+{
+    public class CriarEnderecoDto
+    {
+        public required string rua { get; set; }
+
+        public required string numero { get; set; }
+
+        public required string cep { get; set; }
+
+    }
+}
